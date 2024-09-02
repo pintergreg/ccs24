4dca56cff379a28942e40408d315484df7f5407e
pintergreg <pintergreg@gmail.com>
2024-09-02 11:51:31 +0200

    Refactor QR code creation

--- plot/qr_code.py
+++ plot/qr_code.py
@@ -4,44 +4,31 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
 from qrcode.image.styles.colormasks import SolidFillColorMask
 
-url = "https://arxiv.org/abs/2312.11343"
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_M,
-    box_size=20,
-    border=2,
-)
-qr.add_data(url)
-qr.make(fit=True)
+def create_qr_code(url: str):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_M,
+        box_size=20,
+        border=2,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
 
-img_s = qr.make_image(
-    image_factory=StyledPilImage,
-    module_drawer=RoundedModuleDrawer(),
-    color_mask=SolidFillColorMask(
-        back_color=(24, 29, 55),
-        front_color=(255, 255, 255),
-    ),
-)
-img_s.save("../assets/arxiv_qr_code.png")
+    img = qr.make_image(
+        image_factory=StyledPilImage,
+        module_drawer=RoundedModuleDrawer(),
+        color_mask=SolidFillColorMask(
+            back_color=(24, 29, 55),
+            front_color=(255, 255, 255),
+        ),
+    )
+    return img
 
 
-url = "https://pintergreg.github.io/ccs24"
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_M,
-    box_size=20,
-    border=2,
-)
-qr.add_data(url)
-qr.make(fit=True)
+img_arxiv = create_qr_code("https://arxiv.org/abs/2312.11343")
+img_arxiv.save("../assets/arxiv_qr_code.png")
 
-img_s = qr.make_image(
-    image_factory=StyledPilImage,
-    module_drawer=RoundedModuleDrawer(),
-    color_mask=SolidFillColorMask(
-        back_color=(24, 29, 55),
-        front_color=(255, 255, 255),
-    ),
-)
-img_s.save("../assets/slides_qr_code.png")
+
+img_slides = create_qr_code("https://pintergreg.github.io/ccs24")
+img_slides.save("../assets/slides_qr_code.png")
